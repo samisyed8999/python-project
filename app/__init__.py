166c959359dd8fec104ecb72e9168ede933ed0b8
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-23 03:17:15 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -79,7 +79,7 @@ def register_dashapps(app):
     dashapp1 = dash.Dash(__name__,
                          server=app,
                          url_base_pathname='/dashboard/',
-                         assets_folder=get_root_path(__name__) + '/dashapp1/assets/',
+                         assets_folder=get_root_path(__name__) + '/assets/',
                          meta_tags=[meta_viewport])
     
     dashapp1.layout = html.Div([
