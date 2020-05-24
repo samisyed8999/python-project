1aa9a68044cdaf1fa285f02005efe0f6d4bf2c30
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-24 02:41:41 +0100

    Update webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -19,13 +19,15 @@ server_bp = Blueprint('main', __name__)
 
 @server_bp.route('/')
 def index():
-    return render_template("index.html", title='Home Page')
+    #return render_template("index.html", title='Home Page')
+    return redirect('https://testsami999.herokuapp.com/login/')
 
 
 @server_bp.route('/login/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        #return redirect(url_for('main.index'))
+        return redirect('https://testsami999.herokuapp.com/dashboard/')
 
     form = LoginForm()
     if form.validate_on_submit():
@@ -36,9 +38,10 @@ def login():
 
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('main.index')
-        return redirect(next_page)
+        #if not next_page or url_parse(next_page).netloc != '':
+            #next_page = url_for('main.index')
+        #return redirect(next_page)
+        return redirect('https://testsami999.herokuapp.com/dashboard/')
 
     return render_template('login.html', title='Sign In', form=form)
 
@@ -48,13 +51,15 @@ def login():
 def logout():
     logout_user()
 
-    return redirect(url_for('main.index'))
+    #return redirect(url_for('main.index'))
+    https://testsami999.herokuapp.com/login/
 
 
 @server_bp.route('/register/', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        #return redirect(url_for('main.index'))
+        return redirect('https://testsami999.herokuapp.com/dashboard/')
 
     form = RegistrationForm()
     if form.validate_on_submit():
