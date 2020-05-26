c7cc05bf4009f80870e89b5107928a519b2bb66e
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-26 18:47:16 +0100

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -13,7 +13,7 @@ import numpy as np
 sf.set_data_dir('~/simfin_data/')
 api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
 
-class python:
+def python():
         global df_income
         global ticker
         global df1
