863719a6c3ec2230760a4e1bfd8d5d6cdcfac26a
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-26 19:59:48 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -716,7 +716,7 @@ def register_dashapps(app):
 
         
         
-_protect_dashviews(dashapp1)
+        _protect_dashviews(dashapp1)
 def _protect_dashviews(dashapp):
     for view_func in dashapp.server.view_functions:
         if view_func.startswith(dashapp.config.url_base_pathname):
