c83ff831b7088dca30901193a8bc863b47e6ec28
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-26 17:20:56 +0100

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -13,7 +13,7 @@ import numpy as np
 sf.set_data_dir('~/simfin_data/')
 api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
 
-class python():
+class python:
         df_income = sf.load(dataset='income', variant='annual', market='us',index=[TICKER,])
         df_income = df_income.drop(['Currency', 'SimFinId', 'Fiscal Period','Publish Date', 'Shares (Basic)',
                                     'Abnormal Gains (Losses)','Abnormal Gains (Losses)','Net Extraordinary Gains (Losses)',
@@ -56,7 +56,7 @@ class python():
         df_balance['Fiscal Year']=df_balance['Fiscal Year'].apply(lambda x: round(x, decimals))
         df3 = df_balance.loc[ticker]
 
-class graphs():
+class graphs:
     fig1=make_subplots(specs=[[{"secondary_y": True}]])
     fig1.add_trace(go.Scatter(x=list(df11['Year']), y=list(df11['Revenue']), name="Revenue"))
     fig1.add_trace(go.Scatter(x=list(df11['Year']), y=list(df11['Cost of Revenue']), name="Cost of Revenue"))
