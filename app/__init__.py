ea7c7054ec91c515e69a4efa31a10d8ede778611
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-26 18:52:48 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ from flask import Flask
 from flask.helpers import get_root_path
 from flask_login import login_required
 from config import BaseConfig
-from pyth import python
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
@@ -18,7 +17,6 @@ import numpy as np
 #sf.set_data_dir('~/simfin_data/')
 #api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
 
-python()
 
 def create_app():
     server = Flask(__name__)
@@ -32,6 +30,8 @@ def create_app():
 
 
 def register_dashapps(app):
+    from pyth import python
+    python()
     # Meta tags for viewport responsiveness
     meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
 
