8bf2fa278096fc5e6169c36afc922e68415aef76
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 18:34:04 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 sf.set_data_dir('~/simfin_data/')
 api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
