3e4b24accb2738b0bbe353f295ee7bdf2408daf5
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 01:50:23 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -15,19 +15,20 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()]) # 
     password = PasswordField('Password', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]) # 
+    
     submit = SubmitField('Register')
     
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Username taken.')
+            raise ValidationError('Username taken.') #
             
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Email address already used.')
+            raise ValidationError('Email address already used.') #
