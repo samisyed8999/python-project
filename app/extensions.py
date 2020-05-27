65ec3b01140b88ee3e73ec77d5dc2afd8f8c52bc
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 18:33:05 +0100

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+bootstrap = Bootstrap() 
