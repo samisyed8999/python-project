20d9f76320beef039cc98e1c7a95b95c37b9ecab
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 03:42:46 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -28,10 +28,10 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
     
     class Post(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+        id = db.Column(db.Integer, primary_key=True)
+        body = db.Column(db.String(140))
+        timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        def __repr__(self):
+            return '<Post {}>'.format(self.body)
