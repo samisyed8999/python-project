d2193cdf3c92a2629e9af8c14851234aa4375b07
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 16:05:58 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     #email = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    #email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     #posts = db.relationship('Post', backref='author', lazy='dynamic')
 
