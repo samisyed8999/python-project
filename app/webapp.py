3fb7d19492cbc8f1dac372a0461b6aa94539e07e
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-27 15:00:20 +0100

    Update webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -64,8 +64,9 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         #user = User(username=form.username.data, email=form.email.data)
-        user = User(username=form.username.data)
-        user.set_password(form.password.data)
+        #user = User(username=form.username.data)
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        #user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
 
