025116df6d3436f33daec941b06aa74086d7ec1b
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-28 00:29:49 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -17,10 +17,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
     remember_me = BooleanField('Remember Me')
     
-    def validate_password(self, password):
-        user = User.query.filter_by(password=password.data).first()
-        if user is None:
-            raise ValidationError('Username or password incorrect.')
+
 
 
 class RegistrationForm(FlaskForm):
