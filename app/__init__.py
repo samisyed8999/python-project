ba8cb4ea4c23190fe03222605c8721fc36c00bae
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-06-23 13:15:11 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 from flask_migrate import Migrate
+from textwrap import dedent
 
 def create_app():
     server = Flask(__name__)
